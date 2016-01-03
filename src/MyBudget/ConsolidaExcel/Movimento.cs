ff9b37527737f630f34b57df30ddfb792ca9fecc
Valeriob <valeriob@gmail.com>
2016-01-03 19:46:27 +0100

    ConsolidaSpese

---
+++ src/MyBudget/ConsolidaExcel/Movimento.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using System;
+
+
+namespace ConsolidaExcel
+{
+    public class Movimento
+    {
+        public DateTime Data { get; set; }
+        public string Categoria { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Spesa { get; set; }
+
+        public string DistributionKey { get; set; }
+
+        //public CreateLine ToCreateLine(DateTime timestamp, BudgetId budgetId, string userId, IEnumerable<MyBudget.Projections.Category> categories)
+        //{
+        //    var category = Categoria.Trim().Replace((char)160, ' ');
+        //    var categoryId = categories.FirstOrDefault(d => string.Compare(d.Name, category, true) == 0).Id;
+        //    var expense = new Expense(new Amount(Currencies.Euro(), Spesa), Data, categoryId, Descrizione, DistributionKey);
+
+        //    return new CreateLine
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        Timestamp = timestamp,
+        //        BudgetId = budgetId.ToString(),
+        //        LineId = LineId.Create(budgetId).ToString(),
+        //        UserId = userId,
+        //        Expense = expense,
+
+        //    };
+        //}
+
+        public static Movimento TryParse(IXLRow row)
+        {
+            try
+            {
+                var data = (DateTime)row.Cell("A").Value;
+                var categoria = (string)row.Cell("B").Value;
+                var descrizione = (string)row.Cell("C").Value;
+                var spesa = Convert.ToDecimal(row.Cell("D").Value);
+
+                return new Movimento
+                {
+                   Data = data,
+                   Categoria = categoria,
+                   Descrizione = descrizione,
+                   Spesa = spesa,
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{3} il {0:d} : {1} - {2}", Data, Categoria, Descrizione, Spesa);
+        }
+    }
+}
---
+++ src/MyBudget/ConsolidaExcel/Program.cs
@@ -0,0 +1,108 @@
+﻿using ClosedXML.Excel;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolidaExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileSpese = args[0];
+            var fileAnalisi = args[1];
+            //var file = @"spese.xlsx";
+            //var excel = new ExcelQueryFactory(file);
+            var numeroDiAnniFinoAdOggi = DateTime.Today.Year - 2011;
+            var anni = Enumerable.Range(2011, numeroDiAnniFinoAdOggi);
+
+            using (var analisiWb = new XLWorkbook())
+            {
+                var dati = analisiWb.Worksheets.Add("Dati");
+                int riga = 1;
+                dati.Cell(riga, "A").Value = "Data";
+                dati.Cell(riga, "B").Value = "Categoria";
+                dati.Cell(riga, "C").Value = "Descrizione";
+                dati.Cell(riga, "D").Value = "Spesa";
+                riga++;
+
+                using (var spese = new XLWorkbook(fileSpese))
+                {
+                    foreach (var anno in anni)
+                    {
+                        var wsAnno = spese.Worksheet(anno + "");
+                        var movimenti = wsAnno.Rows().Select(Movimento.TryParse).Where(r => r != null).ToArray();
+
+                        foreach (var movimento in movimenti)
+                        {
+                            dati.Cell(riga, "A").Value = movimento.Data;
+                            dati.Cell(riga, "B").Value = movimento.Categoria;
+                            dati.Cell(riga, "C").Value = movimento.Descrizione;
+                            dati.Cell(riga, "D").Value = movimento.Spesa;
+                            riga++;
+                        }
+
+                    }
+
+                    var dataAsTable = dati.RangeUsed().AsTable();
+                    dati.Tables.Add(dataAsTable);
+                  
+
+                    var range = dati.Range("B1", "B30");
+                    //range = worksheet.RangeUsed();
+
+                    var pivotSh = analisiWb.Worksheets.Add("Pivot");
+                    var pivot = pivotSh.CreatePivotTable(pivotSh.Cell("F2"));
+                    pivot.SourceRange = range;
+        
+                    ////var pivot = worksheet.PivotTables.AddNew("PivotTable", worksheet.Cell("F2"), range);
+                    pivot.RowLabels.Add("Categoria");
+                   // pivot.RowLabels.Add("Descrizione");
+                   // pivot.ColumnLabels.Add("Data");
+                   // pivot.Values.Add("Spesa");
+                  
+
+                }
+
+
+                analisiWb.SaveAs(fileAnalisi);
+
+            }
+
+
+            //using (var workbook = new XLWorkbook())
+            //{
+            //    var worksheet = workbook.Worksheets.Add("Analisi");
+            //    int riga = 1;
+            //    worksheet.Cell(riga, "A").Value = "Data";
+            //    worksheet.Cell(riga, "B").Value = "Categoria";
+            //    worksheet.Cell(riga, "C").Value = "Descrizione";
+            //    worksheet.Cell(riga, "D").Value = "Spesa";
+            //    riga++;
+
+            //    foreach (var anno in anni)
+            //    {
+            //        var movimenti = excel.Worksheet<Movimento>(anno + "").Where(r => r.Data != DateTime.MinValue);
+
+            //        foreach (var movimento in movimenti)
+            //        {
+            //            worksheet.Cell(riga, "A").Value = movimento.Data;
+            //            worksheet.Cell(riga, "B").Value = movimento.Categoria;
+            //            worksheet.Cell(riga, "C").Value = movimento.Descrizione;
+            //            worksheet.Cell(riga, "D").Value = movimento.Spesa;
+            //            riga++;
+            //        }
+
+            //    }
+
+
+            //    workbook.SaveAs("Spese Analisi.xlsx");
+
+            //}
+
+        }
+    }
+}
